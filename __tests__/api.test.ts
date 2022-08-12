73b5130dfcbc6bc944f32d2dee970822831c6aae
Jakob Gaard Andersen <jakobsky@live.dk>
2022-08-12 23:38:46 +0200

    Correct typeerror in tests

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,24 +1,27 @@
 jest.mock('node-fetch');
 
-import fetch from 'node-fetch'
-import API from '../src/api'
-const { Response } = jest.requireActual('node-fetch');
+import fetch from 'node-fetch';
+import API from '../src/api';
+const { Response } = jest.requireActual('node-fetch');
 
 test('should invoke disarm command', async () => {
-	(fetch as unknown as jest.Mock).mockResolvedValue(new Response(undefined, { status: 200}))
+	(fetch as unknown as jest.Mock).mockResolvedValue(
+		new Response(undefined, { status: 200 }),
+	);
 
-	const api = new API('username', 'password')
-	await api.disarmPanel('panelId-123')
+	const api = new API('username', 'password');
+	await api.disarmPanel(103031);
 	expect(fetch).toHaveBeenCalled();
 	expect(fetch).toMatchSnapshot();
-})
+});
 
 test('should invoke arm command', async () => {
-	(fetch as unknown as jest.Mock).mockResolvedValue(new Response(undefined, { status: 200}))
+	(fetch as unknown as jest.Mock).mockResolvedValue(
+		new Response(undefined, { status: 200 }),
+	);
 
-	const api = new API('username', 'password')
-	await api.armPanel('panelId-123')
+	const api = new API('username', 'password');
+	await api.armPanel(103031);
 	expect(fetch).toHaveBeenCalled();
 	expect(fetch).toMatchSnapshot();
-})
-
+});
